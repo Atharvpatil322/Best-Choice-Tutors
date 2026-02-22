b295d8c38138bc000bc595f84652cf5c19a0f8bf
Suyash Bansod <suyash@azafashions.com>
2026-02-23 00:38:12 +0530

    minorrrr

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,27 +5,15 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   chunkSizeWarningLimit: 1600, 
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks(id) {
-  //         if (id.includes('node_modules')) {
-  //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
-  //         }
-  //       },
-  //     },
-  //   },
-  // },
   build: {
-    chunkSizeWarningLimit: 1000,
-    reportCompressedSize: false,
+    chunkSizeWarningLimit: 1600, 
+      reportCompressedSize: false,
     rollupOptions: {
       output: {
-        manualChunks: {
-          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
-          'vendor-ui': ['lucide-react', 'framer-motion', '@radix-ui/react-slot'],
-          'vendor-maps': ['leaflet', 'react-leaflet'],
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
         },
       },
     },
